9ef9d9e8db5115aec9d786d5675b547b6281e316
rootscope <datrootscope@gmail.com>
2014-12-06 19:31:54 -0700

    sidebar cats highlight when selected

--- controllers/catListControllers.js
+++ controllers/catListControllers.js
@@ -1,12 +1,17 @@
 angular.module("catApp")
-.controller("catListCtrl", function($scope, $filter){
+.constant("catListActiveClass", "btn-primary")
+.controller("catListCtrl", function($scope, $filter, catListActiveClass){
   var selectedCategory = null;
-  
+
   $scope.selectCategory = function(newCategory){
     selectedCategory = newCategory;
   }
 
   $scope.categoryFilterFn = function(cat){
     return selectedCategory == null || cat.breed == selectedCategory;
+  }
+
+  $scope.getCategoryClass = function(category){
+    return selectedCategory == category ? catListActiveClass : "";
   }
 });
