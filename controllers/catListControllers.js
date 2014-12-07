c5727ceb0f0290695fb9be8fc2098f618017eaa3
rootscope <datrootscope@gmail.com>
2014-12-07 13:23:59 -0700

    rem home button highlighting

--- controllers/catListControllers.js
+++ controllers/catListControllers.js
@@ -1,33 +1,32 @@
 angular.module("catApp")
-  .constant("catListActiveClass", "btn-success wtf")
-  .constant("pageListActiveClass", "btn-success")
-  .constant("catListPageCount", 3)
-  .controller("catListCtrl", function($scope, $filter,
-    catListActiveClass, pageListActiveClass, catListPageCount){
-    var selectedCategory = null;
+.constant("catListActiveClass", "btn-success wtf")
+.constant("pageListActiveClass", "btn-success")
+.constant("catListPageCount", 3)
+.controller("catListCtrl", function($scope, $filter,
+  catListActiveClass, pageListActiveClass, catListPageCount){
+  var selectedCategory = null;
 
+  $scope.selectedPage = 1;
+  $scope.pageSize = catListPageCount;
+
+  $scope.selectCategory = function(newCategory){
+    selectedCategory = newCategory;
     $scope.selectedPage = 1;
-    $scope.pageSize = catListPageCount;
+  }
 
-    $scope.selectCategory = function(newCategory){
-      selectedCategory = newCategory;
-      $scope.selectedPage = 1;
-    }
+  $scope.selectPage = function(newPage){
+    $scope.selectedPage = newPage;
+  }
 
-    $scope.selectPage = function(newPage){
-      console.log("newPage = ", newPage);
-      $scope.selectedPage = newPage;
-    }
+  $scope.categoryFilterFn = function(cat){
+    return selectedCategory == null || cat.breed == selectedCategory;
+  }
 
-    $scope.categoryFilterFn = function(cat){
-      return selectedCategory == null || cat.breed == selectedCategory;
-    }
+  $scope.getCategoryClass = function(category){
+    return selectedCategory == category ? catListActiveClass : "";
+  }
 
-    $scope.getCategoryClass = function(category){
-      return selectedCategory == category ? catListActiveClass : "";
-    }
-
-    $scope.getPageClass = function(page){
-      return $scope.selectedPage == page ? pageListActiveClass : "";
-    }
-  });
+  $scope.getPageClass = function(page){
+    return $scope.selectedPage == page ? pageListActiveClass : "";
+  }
+});
--- controllers/catapp.js
+++ controllers/catapp.js
@@ -1,47 +1,16 @@
 angular.module("catApp", ["customFilters"]);
 
 angular.module("catApp")
-  .constant("dataUrl", "http://localhost:5500/cats")
-  .controller("catAppCtrl", function($scope, $http, dataUrl) {
+.constant("dataUrl", "http://localhost:5500/cats")
+.controller("catAppCtrl", function($scope, $http, dataUrl) {
 
-    $scope.data = {};
+  $scope.data = {};
 
-    $http.get(dataUrl).success(function(data){
-      $scope.data.cats = data;
-    })
-    .error(function(error){
-      $scope.data.error = error;
-    });
-
-    /*
-    $scope.data = {
-      cats: [{
-        name: "cat #1",
-        desc: "a standard cat",
-        breed: "persian",
-        worth: 100
-      },{
-        name: "cat #2",
-        desc: "a standard cat",
-        breed: "shorthair",
-        worth: 110
-      },{
-        name: "cat #3",
-        desc: "a standard cat",
-        breed: "saimese",
-        worth: 210
-      },{
-        name: "cat #4",
-        desc: "a standard cat",
-        breed: "persian",
-        worth: 290
-      },{
-        name: "cat #5",
-        desc: "a standard cat",
-        breed: "maine coon",
-        worth: 202
-      }]
-    };
-    */
-
+  $http.get(dataUrl).success(function(data){
+    $scope.data.cats = data;
+  })
+  .error(function(error){
+    $scope.data.error = error;
   });
+
+});
