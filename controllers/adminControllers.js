83532e3fb3153adf511975a4b9fa6068ca0314fc
rootscope <datrootscope@gmail.com>
2014-12-12 22:23:34 -0700

    add mainCtrl

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,18 +1,28 @@
 angular.module("catAppAdmin")
 .constant("authUrl", "http://localhost:5500/users/login")
-.controller("authCtrl", function($scope, $http, $location, authUrl) {
-  $scope.authenticate = function(user, pass) {
-    $http.post(authUrl, {
+.controller("authCtrl", function($scope, $http, $location, authUrl){
+  $scope.authenticate = function(user, pass){
+    $http.post(authUrl,{
       username: user,
       password: pass
     }, {
       withCredentials: true
-    }).success(function(data) {
+    }).success(function(data){
       console.log("success");
       $location.path("/main");
-    }).error(function(error) {
+    }).error(function(error){
       console.log("error");
       $scope.authenticationError = error;
     });
   }
+})
+.controller("mainCtrl", function($scope){
+  $scope.screens = ["cats", "orders"];
+  $scope.current = $scope.screens[0];
+  $scope.setScreen = function(index) {
+    $scope.current = $scope.screens[index];
+  };
+  $scope.getScreen = function(){
+    return $scope.current == "cats" ? "/views/adminCats.html" : "/views/adminOrders.html";
+  };
 });
