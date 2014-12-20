090eefbe86bb0d5d19425a0a63ac603ef77dbd4f
rootscope <datrootscope@gmail.com>
2014-12-19 20:25:33 -0700

    add dates display

--- ex.js
+++ ex.js
@@ -1,5 +1,7 @@
 var exApp = angular.module("exApp", []);
-exApp.controller("dayCtrl", function($scope, lol) {
-  $scope.lol = lol;
+exApp.controller("dayCtrl", function($scope) {
+  var days = ["sun","mon","tue","wed","thu","fri","sat"];
+  $scope.day = days[new Date().getDay()];
+  $scope.tomorrow = days[(new Date().getDay() + 1) % 7];
   $scope.wtf = "wtf";
 });
