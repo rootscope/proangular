d82e442b5340a425cabf682098dc2a35a53d65a7
rootscope <datrootscope@gmail.com>
2014-12-19 17:54:04 -0700

    test ng components

--- ex.js
+++ ex.js
@@ -1,4 +1,21 @@
-var myApp = angular.module("exApp", []);
-myApp.controller("dayCtrl", function($scope) {
+var exApp = angular.module("exApp", []);
+exApp.constant("lol", function(){
+  var s = "";
+  for(var i = 0; i < 10; i++){
+    if(i % 3 == 0){
+      s += "f";
+    }
+    else if(i % 2 == 0){
+      s += "t";
+    }
+    else{
+      s += "w";
+    }
+  }
+  return s;
+});
+
+exApp.controller("dayCtrl", function($scope, lol) {
+  $scope.lol = lol;
   $scope.wtf = "wtf";
 });
