772cf8d6286a1ab1190b3e40984aa5324177135a
rootscope <datrootscope@gmail.com>
2014-12-19 18:06:16 -0700

    rem constant component

--- ex.js
+++ ex.js
@@ -1,20 +1,4 @@
 var exApp = angular.module("exApp", []);
-exApp.constant("lol", function(){
-  var s = "";
-  for(var i = 0; i < 10; i++){
-    if(i % 3 == 0){
-      s += "f";
-    }
-    else if(i % 2 == 0){
-      s += "t";
-    }
-    else{
-      s += "w";
-    }
-  }
-  return s;
-});
-
 exApp.controller("dayCtrl", function($scope, lol) {
   $scope.lol = lol;
   $scope.wtf = "wtf";
