00d5ab88cf38c3e570b34baa37e3d09902447a45
rootscope <datrootscope@gmail.com>
2014-12-19 22:14:28 -0700

    chain controllers to exApp module

--- ex.js
+++ ex.js
@@ -1,10 +1,10 @@
-var exApp = angular.module("exApp", []);
-exApp.controller("dayCtrl", function($scope){
+angular.module("exApp", [])
+.controller("dayCtrl", function($scope){
   var days = ["sun","mon","tue","wed","thu","fri","sat"];
   $scope.day = days[new Date().getDay()];
   $scope.wtf = "wtf";
-});
-exApp.controller("tomorrowCtrl", function($scope){
+})
+.controller("tomorrowCtrl", function($scope){
   var days = ["sun","mon","tue","wed","thu","fri","sat"];
   $scope.day = days[(new Date().getDay() + 1) % 7];
 });
