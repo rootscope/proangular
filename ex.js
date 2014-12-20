368e2aa08094771cafc465b7ec0d5457a4ca6032
rootscope <datrootscope@gmail.com>
2014-12-19 22:11:53 -0700

    messing with controllers

--- ex.js
+++ ex.js
@@ -1,7 +1,10 @@
 var exApp = angular.module("exApp", []);
-exApp.controller("dayCtrl", function($scope) {
+exApp.controller("dayCtrl", function($scope){
   var days = ["sun","mon","tue","wed","thu","fri","sat"];
   $scope.day = days[new Date().getDay()];
-  $scope.tomorrow = days[(new Date().getDay() + 1) % 7];
   $scope.wtf = "wtf";
+});
+exApp.controller("tomorrowCtrl", function($scope){
+  var days = ["sun","mon","tue","wed","thu","fri","sat"];
+  $scope.day = days[(new Date().getDay() + 1) % 7];
 });
